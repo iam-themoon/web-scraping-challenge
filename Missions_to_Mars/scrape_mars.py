7d65f7d684f9559a7cd60febc7d7baf5f31854cb
Joshua M <joshua.moon10@gmail.com>
2022-03-16 19:25:32 -0400

    Committing finished jupyter notebook and started python app file

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,17 @@
+# import necessary libraries
+from flask import Flask, render_template
+
+
+# scrape all function
+
+# scrape the mars news page
+
+# scrape through the featured image page
+
+# scrape through the facts page
+
+# scrape through the hemispheres pages
+
+
+if __name__ == "__main__":
+    print(scrape_all())
